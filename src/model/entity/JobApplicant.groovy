dce528cdeefb3f706cadf75c41f4014666bffdb4
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-08-17 12:00:28 -0300

    refactor: move unecessary getters/setters/fields from jobApplicants to Person

--- src/model/entity/JobApplicant.groovy
+++ src/model/entity/JobApplicant.groovy
@@ -1,81 +1,8 @@
 package model.entity
 
 class JobApplicant extends Person {
-    static final MAX_SKILLS = 5
-
     int age
     String CPF
-    Skill[] skills
-
-    JobApplicant() {
-        this.skills = new Skill[MAX_SKILLS]
-    }
-
-    BigInteger getID() {
-        return super.ID
-    }
-
-    void setID(BigInteger ID) {
-       super.ID = ID
-    }
-
-    String getName() {
-        return super.name
-    }
-
-    void setName(String name) {
-        super.name = name
-    }
-
-    String getDescription() {
-        return super.description
-    }
-
-    String getEmail() {
-        return super.email
-    }
-
-    void setEmail(String email) {
-        super.email = email
-    }
-
-    Address getAddress() {
-        return super.address
-    }
-
-    void setAddress(Address address) {
-        super.address = address
-    }
-
-    void addSkill(Skill... skills) {
-        int marker = 0
-        for (Skill skill in skills) {
-            for (int i = marker; i < this.skills.length; i++) {
-                if (this.skills[this.skills.length - 1]) {
-                    throw new IllegalStateException("Container is full (can only contain 5 elements for now)")
-                }
-
-                if (!this.skills[i]) {
-                    this.skills[i] = skill
-                    marker = i
-                    break
-                }
-            }
-        }
-    }
-
-    private String getStringFormatedSkills() {
-        String line = ""
-        for (Skill skill in skills) {
-            if (skill)
-                line += skill.toString() + ", "
-        }
-
-        int lastComma = line.lastIndexOf(", ")
-        line = line.substring(0, lastComma)
-
-        return line
-    }
 
     @Override
     String toString() {
@@ -84,6 +11,6 @@ class JobApplicant extends Person {
             "\nEmail: $email" +
             "\nIdade: $age" +
             "\nAddress: $address" +
-            "\nCompetências: ${getStringFormatedSkills()}\n"
+            "\nCompetências: ${super.getStringFormatedSkills()}\n"
     }
 }
--- src/model/entity/Person.groovy
+++ src/model/entity/Person.groovy
@@ -1,9 +1,46 @@
 package model.entity
 
 abstract class Person {
+    private static final int MAX_SKILLS = 5
+
     BigInteger ID
     String name
     String description
     String email
     Address address
+    Skill[] skills
+
+    Person() {
+        this.skills = new Skill[MAX_SKILLS]
+    }
+
+    void addSkill(Skill... skills) {
+        int marker = 0
+        for (Skill skill in skills) {
+            for (int i = marker; i < this.skills.length; i++) {
+                if (this.skills[this.skills.length - 1]) {
+                    throw new IllegalStateException("Container is full (can only contain 5 elements for now)")
+                }
+
+                if (!this.skills[i]) {
+                    this.skills[i] = skill
+                    marker = i
+                    break
+                }
+            }
+        }
+    }
+
+    String getStringFormatedSkills() {
+        String line = ""
+        for (Skill skill in skills) {
+            if (skill)
+                line += skill.toString() + ", "
+        }
+
+        int lastComma = line.lastIndexOf(", ")
+        line = line.substring(0, lastComma)
+
+        return line
+    }
 }
