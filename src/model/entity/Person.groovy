ec19c7ece957f9b8098958fa35f60851ea423d07
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-08-17 12:08:38 -0300

    fix: change array data structure to LinkedHashSet

--- src/model/entity/Person.groovy
+++ src/model/entity/Person.groovy
@@ -1,41 +1,27 @@
 package model.entity
 
 abstract class Person {
-    private static final int MAX_SKILLS = 5
-
     BigInteger ID
     String name
     String description
     String email
     Address address
-    Skill[] skills
+    Set<Skill> skills
 
     Person() {
-        this.skills = new Skill[MAX_SKILLS]
+        this.skills = new LinkedHashSet()
     }
 
     void addSkill(Skill... skills) {
-        int marker = 0
         for (Skill skill in skills) {
-            for (int i = marker; i < this.skills.length; i++) {
-                if (this.skills[this.skills.length - 1]) {
-                    throw new IllegalStateException("Container is full (can only contain 5 elements for now)")
-                }
-
-                if (!this.skills[i]) {
-                    this.skills[i] = skill
-                    marker = i
-                    break
-                }
-            }
+            this.skills.add(skill)
         }
     }
 
     String getStringFormatedSkills() {
         String line = ""
-        for (Skill skill in skills) {
-            if (skill)
-                line += skill.toString() + ", "
+        for (Skill skill in this.skills) {
+            line += skill.toString() + ", "
         }
 
         int lastComma = line.lastIndexOf(", ")
