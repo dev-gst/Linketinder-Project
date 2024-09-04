f3328d3dfb9f5fcad6b562103a313ecfb7100386
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-09-04 18:42:22 -0300

    chore: add jest to create unit tests

---
+++ Frontend/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
