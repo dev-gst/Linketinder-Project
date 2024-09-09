c551bb5875724e83b0c4aa70c93b0b54d98effaf
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-09-09 09:40:24 -0300

    test: implement tests for getCandidateByEmail in handler

--- Frontend/tests/controllers/CandidateHandler.test.ts
+++ Frontend/tests/controllers/CandidateHandler.test.ts
@@ -2,16 +2,46 @@ import { CandidateHandler } from "../../src/controllers/CandidateHandler";
 import { Candidate } from "../../src/models/Candidate";
 import { CandidateRepository } from "../../src/repositories/CandidateRepository";
 import { CandidateService } from "../../src/services/CandidateService";
+import { CandidateView } from "../../src/views/candidate/CandidateView";
 
 
 describe('Test CandidateHandler', () => {
     let candidateList: Candidate[];
     let mockedCandidateService: jest.Mocked<CandidateService>;
     let mockedCandidateRepository: jest.Mocked<CandidateRepository>;
+    let mockedCandidateView: jest.Mocked<CandidateView>;
     let candidateHandler: CandidateHandler;
-    let mockedCandidate: Candidate;
+    let mockedCandidate: jest.Mocked<Candidate>;
 
     beforeEach(() => {
+        candidateList = [];
+        mockedCandidateRepository = new CandidateRepository(candidateList) as jest.Mocked<CandidateRepository>;
+        mockedCandidateService = new CandidateService(mockedCandidateRepository) as jest.Mocked<CandidateService>;
+        mockedCandidateView = new CandidateView() as jest.Mocked<CandidateView>;
+
+        mockedCandidateService.create = jest.fn();
+        mockedCandidateService.save = jest.fn();
+        mockedCandidateService.getByEmail = jest.fn();
+        mockedCandidateService.getByCPF = jest.fn();
+
+        mockedCandidateView.showProfile = jest.fn();
+
+        candidateHandler = new CandidateHandler(mockedCandidateService, mockedCandidateView);
+
+        mockedCandidate = {
+            id: BigInt(1),
+            name: 'John Doe',
+            email: `johndoe@example.com`,
+            description: `good person`,
+            address: `Good street, 1`,
+            skills: ['JavaScript', 'TypeScript', 'React'],
+            education: 'Computer Engineer',
+            age: 30,
+            CPF: `1234567891`,
+        } as jest.Mocked<Candidate>;
+    })
+
+    test('Receive data, creates and saves a candidate', () => {
         document.body.innerHTML += `
             <form id="candidate-sign-up-form">
                 <input id="candidate-name" value="John Doe">
@@ -24,33 +54,7 @@ describe('Test CandidateHandler', () => {
                 <input id="candidate-cpf" value="1234567891">
                 button id="candidate-sign-up-button" type="submit">Sign Up</button>
             </form>`
-
-        candidateList = [];
-        mockedCandidateRepository = new CandidateRepository(candidateList) as jest.Mocked<CandidateRepository>;
-        mockedCandidateService = new CandidateService(mockedCandidateRepository) as jest.Mocked<CandidateService>;
-
-        mockedCandidateService.create = jest.fn();
-        mockedCandidateService.save = jest.fn();
-
-        candidateHandler = new CandidateHandler(mockedCandidateService);
-        candidateHandler.startListeners();
-
-        mockedCandidate = {
-            id: BigInt(1),
-            name: 'John Doe',
-            email: `johndoe@example.com`,
-            description: `good person`,
-            address: `Good street, 1`,
-            skills: ['JavaScript', 'TypeScript', 'React'],
-            education: 'Computer Engineer',
-            age: 30,
-            CPF: `1234567891`,
-        } as Candidate;
-    })
-
-    test('Receive data, creates and saves a candidate', () => {
-        mockedCandidateService.create.mockReturnValue(mockedCandidate);
-
+        
         const form: HTMLFormElement | null = document.querySelector('#candidate-sign-up-form');
         if (!form) {
             throw new Error('Form not found')
@@ -60,6 +64,9 @@ describe('Test CandidateHandler', () => {
             bubbles: true,
             cancelable: true,
         });
+        
+        candidateHandler.startListeners();
+        mockedCandidateService.create.mockReturnValue(mockedCandidate);
 
         form.dispatchEvent(submitEvent);
 
@@ -77,5 +84,34 @@ describe('Test CandidateHandler', () => {
 
         expect(mockedCandidateService.save).toHaveBeenCalledTimes(1);
         expect(mockedCandidateService.save).toHaveBeenCalledWith(mockedCandidate);
+    });
+
+    test('Get candidate by email', () => {
+        document.body.innerHTML += `
+        <form id="candidate-login-form">
+            <input id="candidate-email-login" value="johndoe@example.com">
+            <button class="login-button" id="candidate-login-button" type="submit">Log In</button>
+        </form>`
+
+        const form: HTMLFormElement | null = document.querySelector('#candidate-login-form');
+        if (!form) {
+            throw new Error('Form not found')
+        }
+
+        const submitEvent: Event = new Event('submit', {
+            bubbles: true,
+            cancelable: true,
+        });
+
+        candidateHandler.startListeners();
+        mockedCandidateService.getByEmail.mockReturnValue(mockedCandidate);
+
+        form.dispatchEvent(submitEvent);
+
+        expect(mockedCandidateService.getByEmail).toHaveBeenCalledTimes(1);
+        expect(mockedCandidateService.getByEmail).toHaveBeenCalledWith('johndoe@example.com');
+    
+        expect(mockedCandidateView.showProfile).toHaveBeenCalledTimes(1);
+        expect(mockedCandidateView.showProfile).toHaveBeenCalledWith(mockedCandidate);
     });
 });
