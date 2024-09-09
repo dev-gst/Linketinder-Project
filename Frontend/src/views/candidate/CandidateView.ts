2549130dd2bf1998a74712b3222b32f9df6b3505
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-09-09 19:19:30 -0300

    feat: start to implement view for the app

---
+++ Frontend/src/views/candidate/CandidateView.ts
@@ -0,0 +1,12 @@
+import { Candidate } from "../../models/Candidate";
+
+export class CandidateView {
+
+    public showProfile(candidate: Candidate): void {
+        
+    }
+
+    private buildProfile(candidate: Candidate): HTMLBodyElement {
+        return new HTMLBodyElement();
+    }
+}
