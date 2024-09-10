c92ba0057e8784b03ec4bbadbd537a983dc8d7d0
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-09-09 23:49:07 -0300

    test: add test for CandidateView

--- Frontend/tests/views/candidate/CandidateView.test.ts
+++ Frontend/tests/views/candidate/CandidateView.test.ts
@@ -1,5 +1,73 @@
+import { Candidate } from "../../../src/models/Candidate";
+import { CandidateView } from "../../../src/views/candidate/CandidateView";
+
 describe('Test CandidateView', () => {
-    test('', () => {
-        
+    let candidate: Candidate;
+    let HTMLText: string;
+    let candidateView: CandidateView;
+
+    beforeEach(() => {
+        candidateView = new CandidateView();
+
+        candidate = new Candidate()
+        candidate.id = BigInt(1);
+        candidate.name = 'John Doe';
+        candidate.description = 'good person';
+        candidate.email = 'johndoe@example.com';
+        candidate.address = 'Good street, 1';
+        candidate.skills = ['JavaScript', 'TypeScript', 'React'];
+        candidate.education = 'Computer Engineering';
+        candidate.age = 30;
+        candidate.CPF = '1234567891';
+
+        HTMLText = `
+        <body>
+            <h1>Profile</h1>
+            <section class="profile" id="candidate-profile">
+                <div class="profile-entry">
+                    <div>Name:</div>
+                    <div>${candidate.name}</div>
+                </div>
+                <div class="profile-entry">
+                    <div>Email:</div>
+                    <div>${candidate.email}</div>
+                </div>
+                <div class="profile-entry">
+                    <div>Bio:</div>
+                    <div>${candidate.description}</div>
+                </div>
+                <div class="profile-entry">
+                    <div>Adress:</div>
+                    <div>${candidate.address}</div>
+                </div>
+                <div class="profile-entry">
+                    <div>Skills:</div>
+                    <div>${candidate.skills}</div>
+                </div>
+                <div class="profile-entry">
+                    <div>Education:</div>
+                    <div>${candidate.education}</div>
+                </div>
+                <div class="profile-entry">
+                    <div>Age:</div>
+                    <div>${candidate.age}</div>
+                </div>
+                <div class="profile-entry">
+                    <div>CPF</div>
+                    <div>${candidate.CPF}</div>
+                </div>
+            </section>
+            <section>
+            </section>
+        </body>
+        `;
+    })
+    
+    test('showProfiles builds and display the candidate profile', () => {
+        candidateView.showProfile(candidate);
+
+        const parsedHTML = new DOMParser().parseFromString(HTMLText, "text/html");
+
+        expect(document.body.innerHTML).toEqual(parsedHTML.body.innerHTML);
     })
 });
