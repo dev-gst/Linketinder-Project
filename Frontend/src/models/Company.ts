76c2c8a690be5f572d498fd166ecaeac8786963a
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-09-10 10:09:15 -0300

    feat: create jobOpening class

--- Frontend/src/models/Company.ts
+++ Frontend/src/models/Company.ts
@@ -1,8 +1,10 @@
+import { JobOpening } from "./JobOpening";
 import {Person} from "./Person"
 
 export class Company extends Person {
     
     private _CNPJ?: string;
+    private _jobOpenings?: JobOpening[];
 
     constructor(
         id?: bigint,
@@ -11,10 +13,12 @@ export class Company extends Person {
         email?: string,
         address?: string,
         CNPJ?: string,
+        jobOpenings?: JobOpening[]
 
     ) {
         super(id, name, description, email, address);
         this._CNPJ = CNPJ;
+        this._jobOpenings = jobOpenings;
     }
 
     public get CNPJ(): string | undefined{
@@ -23,5 +27,13 @@ export class Company extends Person {
 
     public set CNPJ(CNPJ: string | undefined) {
         this._CNPJ = CNPJ;
+    }
+
+    public get jobOpenings(): JobOpening[] | undefined{
+        return this._jobOpenings;
+    }
+
+    public set jobOpenings(jobOpenings: JobOpening[] | undefined) {
+        this._jobOpenings = jobOpenings;
     }
 }
