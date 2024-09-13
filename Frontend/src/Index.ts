7110af0232f7acd31f67296e2303c0e40cf80997
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-09-13 01:51:44 -0300

    feat: add company classes to main

--- Frontend/src/Index.ts
+++ Frontend/src/Index.ts
@@ -1,8 +1,13 @@
 import { CandidateHandler } from "./controllers/CandidateHandler";
+import { CompanyHandler } from "./controllers/CompanyHandler";
 import { Candidate } from "./models/Candidate";
+import { Company } from "./models/Company";
 import { CandidateRepository } from "./repositories/CandidateRepository";
+import { CompanyRepository } from "./repositories/CompanyRepository";
 import { CandidateService } from "./services/CandidateService";
+import { CompanyService } from "./services/CompanyService";
 import { CandidateView } from "./views/candidate/CandidateView";
+import { CompanyView } from "./views/company/CompanyView";
 
 class Main {
 
@@ -13,7 +18,14 @@ class Main {
         const candidateService: CandidateService = new CandidateService(candidateRepository)
         const candidateHandler: CandidateHandler = new CandidateHandler(candidateService, candidateView);
 
+        const companyList: Company[] = [];
+        const companyRepository = new CompanyRepository(companyList);
+        const companyView = new CompanyView();
+        const companyService: CompanyService = new CompanyService(companyRepository)
+        const companyHandler: CompanyHandler = new CompanyHandler(companyService, companyView);
+
         candidateHandler.startListeners();
+        companyHandler.startListeners();
     }
 }
 
