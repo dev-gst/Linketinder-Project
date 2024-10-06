c3bd4b3429c0e6f8ed7a5b45f23417478183f431
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-10-06 03:15:34 -0300

    feat: properly implement DBConnectionManager singleton pattern

--- src/main/groovy/Linketinder/database/DBConnectionManager.groovy
+++ src/main/groovy/Linketinder/database/DBConnectionManager.groovy
@@ -7,8 +7,59 @@ import java.sql.DriverManager
 import java.sql.SQLException
 
 class DBConnectionManager {
+    private static final DBConnectionManager instance
+    private Connection conn
 
-    static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(Env.DB_URL, Env.DB_USER, Env.DB_PASSWORD)
+    static {
+        instance = new DBConnectionManager()
+    }
+
+    private DBConnectionManager() {
+        try {
+            Class.forName("org.postgresql.Driver")
+        } catch (ClassNotFoundException e) {
+
+            throw new ExceptionInInitializerError("Failed to initialize DBConnectionManager: " + e.getMessage())
+        }
+
+        connect()
+    }
+
+    private void connect() {
+        try {
+            this.conn =  DriverManager.getConnection(
+                    Env.DB_URL,
+                    Env.DB_USER,
+                    Env.DB_PASSWORD
+            )
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage())
+            e.printStackTrace()
+
+            throw new RuntimeException("Failed to connect to database", e)
+        }
+    }
+
+    static DBConnectionManager getInstance() {
+
+        return instance
+    }
+
+    void connectionClose() {
+        if (this.conn != null && !this.conn.isClosed()) {
+            try {
+                this.conn.close()
+            } catch (SQLException e) {
+                throw new RuntimeException("Failed to close connection on Database", e)
+            }
+        }
+    }
+
+    Connection getConnection() {
+        if (this.conn == null || this.conn.isClosed()) {
+            connect()
+        }
+
+        return conn
     }
 }
