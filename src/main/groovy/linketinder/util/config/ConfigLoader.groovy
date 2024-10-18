43febeef1d228e2316fa0411aa8f1062cbae7b0c
Gustavo Garcia <gst.garc@gmail.com>
2024-10-18 20:09:50 -0300

    feat: add null checks for ConfigLoader

--- src/main/groovy/linketinder/util/config/ConfigLoader.groovy
+++ src/main/groovy/linketinder/util/config/ConfigLoader.groovy
@@ -8,6 +8,10 @@ class ConfigLoader {
     private Map<String, Map<String, String>> configs
 
     ConfigLoader(YamlParser yamlParser) {
+        if (yamlParser == null) {
+            throw new IllegalArgumentException("YamlParser instance cannot be null")
+        }
+
         this.yamlParser = yamlParser
     }
 
@@ -16,7 +20,12 @@ class ConfigLoader {
             throw new IllegalStateException("Configs have not been loaded")
         }
 
-        return configs.get(firstKey).get(secondKey)
+        String config = configs.get(firstKey).get(secondKey)
+        if (config == null) {
+            throw new IllegalArgumentException("Config with key $firstKey.$secondKey not found")
+        }
+
+        return config
     }
 
     void loadConfigs(String yamlPath) {
--- src/test/groovy/linketinder/util/config/ConfigLoaderTest.groovy
+++ src/test/groovy/linketinder/util/config/ConfigLoaderTest.groovy
@@ -6,7 +6,15 @@ import spock.lang.Specification
 
 class ConfigLoaderTest extends Specification {
 
-    def "loadConfigs works correctly"() {
+    def "constructorThrowsIllegalArgumentExceptionWhenYamlParserIsNull"() {
+        when:
+        new ConfigLoader(null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "loadConfigsWorksCorrectly"() {
         given:
         YamlParser yamlParser = Mock(SnakeYamlParser)
         def configLoader = new ConfigLoader(yamlParser)
@@ -34,5 +42,35 @@ class ConfigLoaderTest extends Specification {
 
         then:
         configLoader.getConfig("firstKey", "secondKey") == "value"
+    }
+
+    def "getConfigThrowsIllegalStateExceptionWhenConfigsNotLoaded"() {
+        given:
+        def configLoader = new ConfigLoader(Mock(YamlParser))
+
+        when:
+        configLoader.getConfig("firstKey", "secondKey")
+
+        then:
+        thrown(IllegalStateException)
+    }
+
+    def "getConfigThrowsIllegalArgumentExceptionWhenConfigNotFound"() {
+        given:
+        Map<String, Map<String, String>> map = new HashMap<String, Map<String, String>>()
+        Map<String, String> innerMap = new HashMap<String, String>()
+        innerMap.put("secondKey", "value")
+        map.put("firstKey", innerMap)
+
+        YamlParser yamlParser = Mock(SnakeYamlParser)
+        ConfigLoader configLoader = new ConfigLoader(yamlParser)
+
+        when:
+        yamlParser.parse("path") >> map
+        configLoader.loadConfigs("path")
+        configLoader.getConfig("firstKey", "thirdKey")
+
+        then:
+        thrown(IllegalArgumentException)
     }
 }
