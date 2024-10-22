484cb99d8c52f3775fde991c7965905ffcb7bf54
Gustavo Garcia <gst.garc@gmail.com>
2024-10-22 01:00:28 -0300

    feat: add more methods of validation for ParamValidation

--- src/main/groovy/main/util/exception/ParamValidation.groovy
+++ src/main/groovy/main/util/exception/ParamValidation.groovy
@@ -5,4 +5,12 @@ class ParamValidation {
     static void requireNonNull(Object obj, String message) {
         if (obj == null) throw new IllegalArgumentException(message)
     }
+
+    static void requireNonBlank(String str, String message) {
+        if (str.isBlank()) throw new IllegalArgumentException(message)
+    }
+
+    static void requirePositive(int num, String message) {
+        if (num <= 0) throw new IllegalArgumentException(message)
+    }
 }
