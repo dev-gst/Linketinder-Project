c577d7d11da9f65e1019b15f3ec4b343459c5adb
Gustavo Garcia <gst.garc@gmail.com>
2024-10-22 00:55:22 -0300

    fix: migrations with wrong credentials

--- src/main/groovy/main/database/FlywayMigration.groovy
+++ src/main/groovy/main/database/FlywayMigration.groovy
@@ -1,20 +1,28 @@
 package main.database
 
+import main.util.config.ConfigLoader
 import main.util.config.Env
+import main.util.parser.yaml.SnakeYamlParser
+import main.util.parser.yaml.YamlParser
 import org.flywaydb.core.Flyway
+import org.yaml.snakeyaml.Yaml
 
 class FlywayMigration {
 
     static void main(String[] args) {
 
+        YamlParser yamlParser = new SnakeYamlParser(new Yaml())
+        ConfigLoader configLoader = new ConfigLoader(yamlParser)
+        Env env = new Env(configLoader)
+
         Flyway flyway = Flyway.configure()
                 .dataSource(
-                        Env.DB_URL,
-                        Env.DB_USER,
-                        Env.DB_PASSWORD
+                        env.getDbUrl(),
+                        env.getDbUser(),
+                        env.getDbPassword()
                 )
-                .locations(Env.FLYWAY_LOCATIONS)
-                .schemas(Env.DB_SCHEMAS)
+                .locations(env.getFlywayLocation())
+                .schemas(env.getDbSchemas())
                 .load()
         flyway.migrate()
     }
