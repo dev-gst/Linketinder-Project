f38987d0fcf7de8171b09995de121a00e7392876
Gustavo Garcia <gst.garc@gmail.com>
2024-10-22 01:02:26 -0300

    feat: implement SearchableInterface in SkillService

--- src/main/groovy/main/services/SkillService.groovy
+++ src/main/groovy/main/services/SkillService.groovy
@@ -3,27 +3,57 @@ package main.services
 import main.models.dtos.skills.SkillDTO
 import main.models.entities.Skill
 import main.repositories.SkillDAO
+import main.services.interfaces.SearchableService
+import main.util.exception.ParamValidation
 
-class SkillService {
+class SkillService implements SearchableService<Skill, SkillDTO> {
 
     SkillDAO skillDAO
 
     SkillService(SkillDAO skillDAO) {
-        Objects.requireNonNull(skillDAO, "SkillDAO cannot be null")
+        ParamValidation.requireNonNull(skillDAO, "SkillDAO cannot be null")
 
         this.skillDAO = skillDAO
     }
 
-    Skill getSkillByName(String skillName) {
-        Objects.requireNonNull(skillName, "Skill name cannot be null")
-        if (skillName.isBlank()) throw new IllegalArgumentException("Skill name cannot be blank")
+    @Override
+    Skill getById(int id) {
+        ParamValidation.requirePositive(id, "Skill ID must be greater than 0")
 
-        return skillDAO.getByName(skillName)
+        return skillDAO.getById(id)
     }
 
-    Set<Skill> saveAll(Set<SkillDTO> skillDTOSet) {
-        Objects.requireNonNull(skillDTOSet, "SkillDTO set cannot be null")
+    @Override
+    Set<Skill> getAll() {
+        return skillDAO.getAll()
+    }
 
+    @Override
+    Set<Skill> findByField(String fieldName, String fieldValue) {
+        ParamValidation.requireNonNull(fieldName, "Field name cannot be null")
+        ParamValidation.requireNonNull(fieldValue, "Field value cannot be null")
+        ParamValidation.requireNonBlank(fieldName, "Field name cannot be blank")
+        ParamValidation.requireNonBlank(fieldValue, "Field value cannot be blank")
+
+        return skillDAO.findByField(fieldName, fieldValue)
+    }
+
+    @Override
+    Skill save(SkillDTO skillDTO) {
+        ParamValidation.requireNonNull(skillDTO, "SkillDTO cannot be null")
+
+        Skill skill = findByField("name", skillDTO.getName())[0]
+        if (skill != null) {
+            return skill
+        } else {
+            int newSkillId = skillDAO.save(skillDTO)
+            return new Skill(newSkillId, skillDTO.getName())
+        }
+    }
+
+    @Override
+    Set<Skill> saveAll(Set<SkillDTO> skillDTOSet) {
+        ParamValidation.requireNonNull(skillDTOSet, "SkillDTO set cannot be null")
         if (skillDTOSet.isEmpty()) throw new IllegalArgumentException("SkillDTO set cannot be empty")
 
         Set<Skill> skills = new LinkedHashSet<>()
@@ -34,16 +64,24 @@ class SkillService {
         return skills
     }
 
-    Skill save(SkillDTO skillDTO) {
-        Objects.requireNonNull(skillDTO, "SkillDTO cannot be null")
+    @Override
+    Skill updateById(int id, SkillDTO skillDTO) {
+        ParamValidation.requireNonNull(skillDTO, "SkillDTO cannot be null")
+        if (id <= 0) throw new IllegalArgumentException("Skill ID must be greater than 0")
 
-        Skill skill = getSkillByName(skillDTO.getName())
-        if (skill != null) {
-            return skill
-        } else {
-            int newSkillId = skillDAO.save(skillDTO)
+        Skill skill = getById(id)
+        if (skill == null) throw new IllegalArgumentException("Skill with ID $id not found")
 
-            return new Skill(newSkillId, skillDTO.getName())
-        }
+        skill.setName(skillDTO.getName())
+        skillDAO.updateById(id, skillDTO)
+
+        return skill
+    }
+
+    @Override
+    void deleteById(int id) {
+        if (id <= 0) throw new IllegalArgumentException("Skill ID must be greater than 0")
+
+        skillDAO.deleteById(id)
     }
 }
