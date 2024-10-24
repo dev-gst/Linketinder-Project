0c61718c46082276fc00f38bd10b85658d39d431
Gustavo Garcia <gst.garc@gmail.com>
2024-10-24 20:45:47 -0300

    feat: create method getByEntityId(int entityId, Class<?> entityClazz) in the SearchableService interface

--- src/main/groovy/main/services/interfaces/SearchableService.groovy
+++ src/main/groovy/main/services/interfaces/SearchableService.groovy
@@ -4,4 +4,5 @@ interface SearchableService<ENTITY, DTO> extends Service<ENTITY, DTO> {
 
     Set<ENTITY> getByField(String fieldName, String fieldValue)
 
+    Set<ENTITY> getByEntityId(int entityId, Class<?> entityClazz)
 }
