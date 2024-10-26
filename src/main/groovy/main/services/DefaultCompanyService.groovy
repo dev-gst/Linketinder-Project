a796b3ee30c521e4a2712bbe9d04d5eba78e5d93
Gustavo Garcia <gst.garc@gmail.com>
2024-10-26 19:47:12 -0300

    feat: implement DefaultCompanyService

---
+++ src/main/groovy/main/services/DefaultCompanyService.groovy
@@ -0,0 +1,125 @@
+package main.services
+
+import main.models.dtos.request.company.CompanyDTO
+import main.models.entities.JobOpening
+import main.models.entities.address.Address
+import main.models.entities.company.Company
+import main.repositories.CompanyDAO
+import main.services.interfaces.AddressService
+import main.services.interfaces.CompanyService
+import main.util.exception.ParamValidation
+import main.util.exception.custom.EntityNotFoundException
+import main.util.exception.custom.NullCollectionException
+
+class DefaultCompanyService implements CompanyService {
+
+    private static final String EMAIL_FIELD = "email"
+
+    CompanyDAO companyDAO
+
+    AddressService addressService
+
+    DefaultCompanyService(CompanyDAO companyDAO, AddressService addressService) {
+        this.companyDAO = companyDAO
+        this.addressService = addressService
+    }
+
+    @Override
+    Company getById(int id) {
+        ParamValidation.requirePositive(id, "id cannot be negative")
+
+        return companyDAO.getById(id)
+    }
+
+    @Override
+    Set<Company> getByField(String fieldName, String fieldValue) {
+        ParamValidation.requireNonBlank(fieldName, "fieldName cannot be null or blank")
+        ParamValidation.requireNonBlank(fieldValue, "fieldValue cannot be null or blank")
+
+        return companyDAO.getByField(fieldName, fieldValue)
+    }
+
+    @Override
+    Set<Company> getByEntityId(int entityId, Class<?> entityClazz) {
+        ParamValidation.requirePositive(entityId, "entityId cannot be negative")
+        ParamValidation.requireNonNull(entityClazz, "entityClazz cannot be null")
+
+        switch (entityClazz) {
+            case JobOpening.class:
+                return companyDAO.getByJobOpeningId(entityId)
+            default:
+                throw new ClassNotFoundException("Class not found for this context")
+        }
+    }
+
+    @Override
+    Set<Company> getAll() {
+        return companyDAO.getAll()
+    }
+
+    @Override
+    int save(CompanyDTO companyDTO) {
+        ParamValidation.requireNonNull(companyDTO, "companyDTO cannot be null")
+
+        Set<Company> foundCompanies = getByField(EMAIL_FIELD, companyDTO.loginDetails.email)
+        if (foundCompanies == null) throw new NullCollectionException("foundCompanies cannot be null")
+
+        Company foundCompany = foundCompanies.isEmpty() ? null : foundCompanies[0]
+        if (foundCompany != null) {
+            return foundCompany.id
+        }
+
+        int addressId = addressService.save(companyDTO.companyDetailsDTO.addressDTO)
+
+        return companyDAO.save(companyDTO, addressId)
+    }
+
+    @Override
+    Set<Integer> saveAll(Set<CompanyDTO> companyDTOS) {
+        ParamValidation.requireNonNull(companyDTOS, "companyDTOS cannot be null")
+
+        Set<Integer> ids = new LinkedHashSet<>()
+        for (CompanyDTO companyDTO : companyDTOS) {
+            ids.add(Integer.valueOf(save(companyDTO)))
+        }
+
+        return ids
+    }
+
+    @Override
+    Company updateById(int id, CompanyDTO companyDTO) {
+        ParamValidation.requirePositive(id, "id cannot be negative")
+        ParamValidation.requireNonNull(companyDTO, "companyDTO cannot be null")
+        if (getById(id) == null) throw new EntityNotFoundException("Company with ID ${id} not found")
+
+        Set<Address> foundAddresses = addressService.getByEntityId(id, Company.class)
+        if (foundAddresses == null) throw new NullCollectionException("foundAddresses cannot be null")
+
+        Address address = foundAddresses.isEmpty() ? null : foundAddresses[0]
+        if (address == null) {
+
+            return updateWhenAddressIsNull(id, companyDTO)
+        }
+
+        return updateWhenAddressIsNotNull(id, companyDTO, address)
+    }
+
+    private Company updateWhenAddressIsNull(int id, CompanyDTO companyDTO) {
+        int addressId = addressService.save(companyDTO.companyDetailsDTO.addressDTO)
+
+        return companyDAO.updateById(id, companyDTO, addressId)
+    }
+
+    private Company updateWhenAddressIsNotNull(int id, CompanyDTO companyDTO, Address address) {
+        addressService.updateById(address.id, companyDTO.companyDetailsDTO.addressDTO)
+
+        return companyDAO.updateById(id, companyDTO, address.id)
+    }
+
+    @Override
+    void deleteById(int id) {
+        ParamValidation.requirePositive(id, "id cannot be negative")
+
+        companyDAO.deleteById(id)
+    }
+}
---
+++ src/test/groovy/main/services/DefaultCompanyServiceTest.groovy
@@ -0,0 +1,296 @@
+package main.services
+
+import main.models.dtos.request.company.CompanyDTO
+import main.models.entities.JobOpening
+import main.models.entities.address.Address
+import main.models.entities.company.Company
+import main.repositories.CompanyDAO
+import main.util.exception.custom.EntityNotFoundException
+import mocks.CompanyMock
+import spock.lang.Specification
+
+class DefaultCompanyServiceTest extends Specification {
+
+    Company companyMock1
+    Company companyMock2
+    CompanyDTO companyDTOMock1
+    CompanyDTO companyDTOMock2
+    CompanyDAO companyDAOMock
+    DefaultAddressService addressServiceMock
+    DefaultCompanyService companyService
+
+    void setup() {
+        CompanyMock companyMockBuilder = new CompanyMock()
+
+        companyMock1 = companyMockBuilder.createCompanyMock(1)
+        companyMock2 = companyMockBuilder.createCompanyMock(2)
+
+        companyDTOMock1 = companyMockBuilder.createCompanyDTOMock(1)
+        companyDTOMock2 = companyMockBuilder.createCompanyDTOMock(2)
+
+        companyDAOMock = Mock(CompanyDAO)
+        addressServiceMock = Mock(DefaultAddressService)
+
+        companyService = new DefaultCompanyService(companyDAOMock, addressServiceMock)
+    }
+
+
+    def "get by id returns correct company"() {
+        given:
+        companyDAOMock.getById(1) >> companyMock1
+        Company expected = companyMock1
+
+        when:
+        Company result = companyService.getById(1)
+
+        then:
+        expected == result
+        1 * companyDAOMock.getById(1) >> companyMock1
+    }
+
+    def "get by id throws exception when id is negative"() {
+        when:
+        companyService.getById(-1)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "get by entity id returns companies by JobOpening class"() {
+        given:
+        Set<Company> expected = new LinkedHashSet<>()
+        expected.add(companyMock1)
+        companyDAOMock.getByJobOpeningId(1) >> expected
+
+        when:
+        Set<Company> result = companyService.getByEntityId(1, JobOpening.class)
+
+        then:
+        result.size() == 1
+        result.contains(companyMock1)
+        1 * companyDAOMock.getByJobOpeningId(1) >> expected
+    }
+
+    def "get by entity id throws exception for invalid class"() {
+        when:
+        companyService.getByEntityId(1, String.class)
+
+        then:
+        thrown(ClassNotFoundException)
+    }
+
+    def "get by entity id throws exception when entity id is negative"() {
+        when:
+        companyService.getByEntityId(-1, JobOpening.class)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "get by entity id throws exception when entity class is null"() {
+        when:
+        companyService.getByEntityId(1, null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "get by field returns companies"() {
+        given:
+        Set<Company> expected = new LinkedHashSet<>()
+        expected.add(companyMock1)
+        expected.add(companyMock2)
+        companyDAOMock.getByField("name", "company") >> expected
+
+        when:
+        Set<Company> result = companyService.getByField("name", "company")
+
+        then:
+        result.size() == 2
+        result.contains(companyMock1)
+        result.contains(companyMock2)
+        1 * companyDAOMock.getByField("name", "company") >> expected
+    }
+
+    def "get by field throws exception when field name is null"() {
+        when:
+        companyService.getByField(null, "company")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "get by field throws exception when field name is blank"() {
+        when:
+        companyService.getByField("", "company")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "get by field throws exception when field value is null"() {
+        when:
+        companyService.getByField("name", null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "get by field throws exception when field value is blank"() {
+        when:
+        companyService.getByField("name", "")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "get all returns all companies"() {
+        given:
+        Set<Company> expected = new LinkedHashSet<>()
+        expected.add(companyMock1)
+        expected.add(companyMock2)
+        companyDAOMock.getAll() >> expected
+
+        when:
+        Set<Company> result = companyService.getAll()
+
+        then:
+        expected == result
+        1 * companyDAOMock.getAll() >> expected
+    }
+
+    def "save returns newly saved company id"() {
+        given:
+        companyDAOMock.getByField("email", "joe1@email.com") >> new LinkedHashSet<>()
+        addressServiceMock.save(companyDTOMock1.companyDetailsDTO.addressDTO) >> 1
+        companyDAOMock.save(companyDTOMock1, 1) >> 1
+
+        when:
+        int result = companyService.save(companyDTOMock1)
+
+        then:
+        1 == result
+    }
+
+    def "save returns already saved company id"() {
+        given:
+        companyMock1.id >> 1
+        Set<Company> foundCompanies = new LinkedHashSet<>()
+        foundCompanies.add(companyMock1)
+        companyDAOMock.getByField("email", "joe1@email.com") >> foundCompanies
+
+        when:
+        int result = companyService.save(companyDTOMock1)
+
+        then:
+        1 == result
+    }
+
+    def "save throws exception when companyDTO is null"() {
+        when:
+        companyService.save(null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "save all returns newly saved companies ids"() {
+        given:
+        companyDAOMock.getByField("email", "joe1@email.com") >> new LinkedHashSet<>()
+        addressServiceMock.save(companyDTOMock1.companyDetailsDTO.addressDTO) >> 1
+        companyDAOMock.save(companyDTOMock1, 1) >> 1
+
+        companyDAOMock.getByField("email", "joe2@email.com") >> new LinkedHashSet<>()
+        addressServiceMock.save(companyDTOMock2.companyDetailsDTO.addressDTO) >> 2
+        companyDAOMock.save(companyDTOMock2, 2) >> 2
+
+        Set<CompanyDTO> companyDTOS = new LinkedHashSet<>()
+        companyDTOS.add(companyDTOMock1)
+        companyDTOS.add(companyDTOMock2)
+
+        when:
+        Set<Integer> result = companyService.saveAll(companyDTOS)
+
+        then:
+        result.size() == 2
+        result.contains(1)
+        result.contains(2)
+    }
+
+    def "update by id returns updated company"() {
+        given:
+        Set<Address> foundAddresses = new LinkedHashSet<>()
+        foundAddresses.add(companyMock1.companyDetails.address)
+
+        companyDAOMock.getById(1) >> companyMock1
+        addressServiceMock.getByEntityId(1, Company.class) >> foundAddresses
+        addressServiceMock.updateById(
+                companyMock1.companyDetails.address.id, companyDTOMock1.companyDetailsDTO.addressDTO
+        ) >> companyMock1.companyDetails.address
+
+        companyDAOMock.updateById(companyMock1.id, companyDTOMock1, companyMock1.companyDetails.address.id) >> companyMock1
+
+        when:
+        Company result = companyService.updateById(1, companyDTOMock1)
+
+        then:
+        result == companyMock1
+    }
+
+    def "update by id returns updated company with address not found"() {
+        given:
+        companyDAOMock.getById(1) >> companyMock1
+        addressServiceMock.getByEntityId(1, Company.class) >> new LinkedHashSet<>()
+        addressServiceMock.save(companyDTOMock1.companyDetailsDTO.addressDTO) >> 1
+        companyDAOMock.updateById(1, companyDTOMock1, 1) >> companyMock1
+
+        when:
+        Company result = companyService.updateById(1, companyDTOMock1)
+
+        then:
+        result == companyMock1
+    }
+
+    def "update by id throws exception when id is negative"() {
+        when:
+        companyService.updateById(-1, companyDTOMock1)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "update by id throws exception when companyDTO is null"() {
+        when:
+        companyService.updateById(1, null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "update by id throws exception when company not found"() {
+        given:
+        companyDAOMock.getById(1) >> null
+
+        when:
+        companyService.updateById(1, companyDTOMock1)
+
+        then:
+        thrown(EntityNotFoundException)
+    }
+
+    def "delete by id deletes company"() {
+        when:
+        companyService.deleteById(1)
+
+        then:
+        1 * companyDAOMock.deleteById(1)
+    }
+
+    def "delete by id throws exception when id is negative"() {
+        when:
+        companyService.deleteById(-1)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+}
