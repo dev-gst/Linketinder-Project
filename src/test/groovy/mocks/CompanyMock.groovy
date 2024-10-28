350eedbbdd9fd7fded0c77163685a0d23e944874
Gustavo Garcia <gst.garc@gmail.com>
2024-10-28 18:20:19 -0300

    fix: adapt CompanyMock for new Company Builder patter

--- src/test/groovy/mocks/CompanyMock.groovy
+++ src/test/groovy/mocks/CompanyMock.groovy
@@ -1,10 +1,7 @@
 package mocks
 
-
-import main.models.dtos.request.company.CompanyDTO
-import main.models.dtos.request.company.CompanyDetailsDTO
-import main.models.entities.company.Company
-import main.models.entities.company.CompanyDetails
+import main.models.dtos.request.CompanyDTO
+import main.models.entities.Company
 import spock.lang.Specification
 
 class CompanyMock extends Specification {
@@ -12,41 +9,25 @@ class CompanyMock extends Specification {
     CompanyDTO createCompanyDTOMock(int n) {
         CompanyDTO companyDTO = Mock(CompanyDTO)
 
-        companyDTO.companyDetailsDTO >> createCompanyDetailsDTOMock(n)
+        companyDTO.name >> "Company $n"
         companyDTO.loginDetails >> new LoginDetailsMock().createLoginDetailsDTOMock(n)
+        companyDTO.description >> "Company description $n"
+        companyDTO.cnpj >> "12345678901234 $n"
+        companyDTO.addressId >> n
 
         return companyDTO
-    }
-
-    CompanyDetailsDTO createCompanyDetailsDTOMock(int n) {
-        CompanyDetailsDTO companyDetailsDTO = Mock(CompanyDetailsDTO)
-
-        companyDetailsDTO.name >> "Company $n"
-        companyDetailsDTO.description >> "Company description $n"
-        companyDetailsDTO.cnpj >> "12345678901234 $n"
-        companyDetailsDTO.addressDTO >> new AddressMock().createAddressDTOMock(n)
-
-        return companyDetailsDTO
     }
 
     Company createCompanyMock(int n) {
         Company company = Mock(Company)
 
         company.id >> n
-        company.companyDetails >> createCompanyDetailsMock(n)
         company.loginDetails >> new LoginDetailsMock().createLoginDetailsMock(n)
+        company.name >> "Company $n"
+        company.description >> "Company description $n"
+        company.cnpj >> "12345678901234 $n"
+        company.addressId >> n
 
         return company
-    }
-
-    CompanyDetails createCompanyDetailsMock(int n) {
-        CompanyDetails companyDetails = Mock(CompanyDetails)
-
-        companyDetails.name >> "Company $n"
-        companyDetails.description >> "Company description $n"
-        companyDetails.cnpj >> "12345678901234 $n"
-        companyDetails.address >> new AddressMock().createAddressMock(n)
-
-        return companyDetails
     }
 }
