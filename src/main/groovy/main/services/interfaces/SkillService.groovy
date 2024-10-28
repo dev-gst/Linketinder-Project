f4924a0e0f2e6e75213aebec40fd088b4351170a
Gustavo Garcia <gst.garc@gmail.com>
2024-10-28 02:05:02 -0300

    feat: create SkillService interface

---
+++ src/main/groovy/main/services/interfaces/SkillService.groovy
@@ -0,0 +1,8 @@
+package main.services.interfaces
+
+import main.models.dtos.request.SkillDTO
+import main.models.entities.Skill
+
+interface SkillService extends SearchableService<Skill, SkillDTO> {
+
+}
