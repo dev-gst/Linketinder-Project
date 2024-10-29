cb079c7d9dd604f4a1f359fefa32ca3906b41ec9
Gustavo Garcia <gst.garc@gmail.com>
2024-10-28 23:29:35 -0300

    feat: reimplement Main class

--- src/main/groovy/main/Main.groovy
+++ src/main/groovy/main/Main.groovy
@@ -2,34 +2,44 @@ package main
 
 import main.database.DBConnectionManager
 import main.repositories.*
-import main.services.CandidateService
-import main.services.CompanyService
-import main.services.JobOpeningService
-import main.services.SkillService
+import main.repositories.interfaces.*
+import main.services.*
+import main.services.interfaces.*
 import main.ui.MainMenu
+import main.util.config.ConfigLoader
+import main.util.config.Env
+import main.util.parser.yaml.SnakeYamlParser
+import main.util.parser.yaml.YamlParser
+import org.yaml.snakeyaml.Yaml
 
 import java.sql.Connection
 
 class Main {
     static void main(String[] args) {
-        DBConnectionManager dbConnectionManager = DBConnectionManager.getInstance()
+        YamlParser yamlParser = new SnakeYamlParser(new Yaml())
+        ConfigLoader configLoader = new ConfigLoader(yamlParser)
+        Env env = new Env(configLoader)
+
+        DBConnectionManager dbConnectionManager = new DBConnectionManager(env)
         Connection connection = dbConnectionManager.getConnection()
 
-        AddressDAO addressDAO = new AddressDAO(connection)
-        SkillDAO skillDAO = new SkillDAO(connection)
-        CandidateDAO candidateDAO = new CandidateDAO(connection)
-        CompanyDAO companyDAO = new CompanyDAO(connection)
-        JobOpeningDAO jobOpeningDAO = new JobOpeningDAO(connection)
+        AddressDAO addressDAO = new DefaultAddressDAO(connection)
+        SkillDAO skillDAO = new DefaultSkillDAO(connection)
+        CandidateDAO candidateDAO = new DefaultCandidateDAO(connection)
+        CompanyDAO companyDAO = new DefaultCompanyDAO(connection)
+        JobOpeningDAO jobOpeningDAO = new DefaultJobOpeningDAO(connection)
 
-        SkillService skillService = new SkillService(skillDAO)
-        CandidateService candidateService = new CandidateService(candidateDAO, addressDAO, skillDAO, skillService)
-        CompanyService companyService = new CompanyService(companyDAO, jobOpeningDAO, addressDAO, skillDAO)
-        JobOpeningService jobOpeningService = new JobOpeningService(jobOpeningDAO, skillDAO, skillService, companyService, addressDAO)
+        AddressService addressService = new DefaultAddressService(addressDAO)
+        SkillService skillService = new DefaultSkillService(skillDAO)
+        CandidateService candidateService = new DefaultCandidateService(candidateDAO)
+        CompanyService companyService = new DefaultCompanyService(companyDAO)
+        JobOpeningService jobOpeningService = new DefaultJobOpeningService(jobOpeningDAO)
 
         MainMenu mainMenu = new MainMenu(
                 companyService,
                 candidateService,
                 jobOpeningService,
+                addressService,
                 skillService
         )
 
