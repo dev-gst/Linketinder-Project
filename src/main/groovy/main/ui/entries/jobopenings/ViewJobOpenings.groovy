a138a88966c8b2ff9336fd1de99da34dee861e62
Gustavo Garcia <gst.garc@gmail.com>
2024-11-04 13:28:43 -0300

    feat: create menu entry to view all job openings

---
+++ src/main/groovy/main/ui/entries/jobopenings/ViewJobOpenings.groovy
@@ -0,0 +1,63 @@
+package main.ui.entries.jobopenings
+
+import main.models.entities.Address
+import main.models.entities.JobOpening
+import main.services.interfaces.AddressService
+import main.services.interfaces.JobOpeningService
+import main.ui.interfaces.MenuCommand
+import main.util.exception.ParamValidation
+
+class ViewJobOpenings implements MenuCommand {
+
+    JobOpeningService jobOpeningService
+    AddressService addressService
+
+    ViewJobOpenings(JobOpeningService jobOpeningService, AddressService addressService) {
+        ParamValidation.requireNonNull(jobOpeningService, "JobOpeningService cannot be null")
+        ParamValidation.requireNonNull(addressService, "AddressService cannot be null")
+
+        this.jobOpeningService = jobOpeningService
+        this.addressService = addressService
+    }
+
+    @Override
+    void execute() {
+        checkForJobOpenings()
+    }
+
+    @Override
+    String getEntryName() {
+        return "Visualizar vagas"
+    }
+
+    private checkForJobOpenings() {
+        Set<JobOpening> jobOpenings = jobOpeningService.getAll()
+        if (jobOpenings.isEmpty()) {
+            println "Nenhuma vaga disponível no momento."
+        } else {
+            viewJobOpenings(jobOpenings)
+        }
+    }
+
+    private void viewJobOpenings(Set<JobOpening> jobOpenings) {
+        println "***** Vagas Disponíveis *****"
+        jobOpenings.each { jobOpening ->
+            println "Nome: ${jobOpening.name}"
+            println "Descrição: ${jobOpening.description}"
+            println "${jobOpening.isRemote ? 'Remota' : 'Presencial'}"
+            println "${jobOpening.isOpen ? 'Aberta' : 'Fechada'}"
+            println "Endereço: ${getJobOpeningAddress(jobOpening)}"
+            println()
+        }
+    }
+
+    private String getJobOpeningAddress(JobOpening jobOpening) {
+        Set<Address> addresses = addressService.getByEntityId(jobOpening.id, JobOpening.class)
+        if (addresses.isEmpty()) {
+            return "Endereço não disponível"
+        }
+
+        Address address = addresses.first()
+        return "${address.city}, ${address.region} - ${address.country}"
+    }
+}
