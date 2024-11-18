ebee184e70515af67cfdd90aca6f63d2241e0061
Gustavo Garcia <gst.garc@gmail.com>
2024-11-18 17:33:50 -0300

    feat: implement controller for JobOpening

---
+++ src/main/groovy/application/controllers/JobOpeningController.groovy
@@ -0,0 +1,63 @@
+package application.controllers
+
+
+import application.models.dtos.request.JobOpeningDTO
+import application.services.ServiceFactory
+import application.services.interfaces.JobOpeningService
+import application.utils.parsers.json.JsonParser
+import application.utils.parsers.json.JsonParserFactory
+import jakarta.servlet.ServletConfig
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+
+@WebServlet(urlPatterns = ["/job-openings/*"])
+class JobOpeningController extends HttpServlet {
+
+    private JobOpeningService jobOpeningService
+    private JsonParser jsonParser
+
+    @Override
+    void init(ServletConfig config) {
+        super.init(config)
+
+        this.jobOpeningService = ServiceFactory.createJobOpeningService()
+        this.jsonParser = JsonParserFactory.createJacksonJsonParser()
+    }
+
+    @Override
+    void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setContentType("application/json")
+        String pathInfo = req.getPathInfo()
+
+        if (pathInfo == null || "/" == pathInfo) {
+            createJobOpening(req, resp)
+        } else {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND)
+        }
+    }
+
+    private void createJobOpening(HttpServletRequest req, HttpServletResponse resp) {
+        BufferedReader reader = req.getReader()
+        StringBuilder requestBody = new StringBuilder()
+
+        String line
+        while ((line = reader.readLine()) != null) {
+            requestBody.append(line)
+        }
+
+        JobOpeningDTO jobOpeningDTO
+        try {
+            jobOpeningDTO = jsonParser.fromJson(requestBody.toString(), JobOpeningDTO.class)
+        } catch (Exception ignored) {
+            resp.writer.write("Invalid request body")
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST)
+            return
+        }
+
+        int id = jobOpeningService.save(jobOpeningDTO)
+        resp.writer.write(jsonParser.toJson(id))
+        resp.setStatus(HttpServletResponse.SC_CREATED)
+    }
+}
