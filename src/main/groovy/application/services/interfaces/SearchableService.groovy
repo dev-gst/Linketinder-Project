7a6f19c6432c7744c0941bc9f737c6770a0ea6ad
Gustavo Garcia <gst.garc@gmail.com>
2024-11-26 01:37:36 -0300

    refac: delete SearchableService interface and transfer its methods to Service

--- src/main/groovy/application/services/interfaces/SearchableService.groovy
+++
@@ -1,8 +0,0 @@
-package application.services.interfaces
-
-interface SearchableService<ENTITY, DTO> extends Service<ENTITY, DTO> {
-
-    Set<ENTITY> getByField(String fieldName, String fieldValue)
-
-    Set<ENTITY> getByEntityId(int entityId, Class<?> entityClazz)
-}
